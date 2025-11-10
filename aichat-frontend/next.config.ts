e9bd758410f4c58ac25eeca7b49edf9f2125129f
Kunal Sable <118177615+Kunals990@users.noreply.github.com>
2025-11-10 16:49:47 +0530

    fix(config): Add API rewrites to next.config.ts for backend URL handling, removed vercel.json - not required

--- aichat-frontend/next.config.ts
+++ aichat-frontend/next.config.ts
@@ -1,14 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "**.googleusercontent.com",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+      },
+    ],
+  },
+  
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
